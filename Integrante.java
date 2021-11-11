af9ca52233ff4aa751e3fbeb9f0ade3422ead464
Alan Miam <89094977+alanMiam@users.noreply.github.com>
2021-11-10 20:07:47 -0600

    TDD4
    Se añadieron pruebas y funcionalidad para el nuevo tipo de integrante "Intendencia", se modificó la clase Integrante para que la Matricula sea un String.

--- Integrante.java
+++ Integrante.java
@@ -1,26 +1,33 @@
 public class Integrante {
-    int matricula = 0;
     int numeroIntegrante = 0;
+    String matriculaStr ="";
     
     public  String  metodo( int numInte,  int valNum){
-        String retorno="";
+        String retorno="ERROR";
+        String matriculaSt;
         
         if (numInte==2 && valNum>=100000 && valNum <=999999) {
+            matriculaSt="al"+valNum;
             retorno="Alumno";
         }else if (numInte==1 && valNum>=1000 && valNum <=9999) {
+            matriculaSt=valNum+"";
             retorno="Maestro";
+        }else if (numInte==3 && valNum>=1 && valNum<=99) {
+            matriculaSt="in"+valNum;
+            retorno="Intendencia";
         }
+        
         return retorno;
     }
+
+    public String getMatriculaStr() {
+        return matriculaStr;
+    }
+
+    public void setMatriculaStr(String matriculaStr) {
+        this.matriculaStr = matriculaStr;
+    }
     
-
-    public int getMatricula() {
-        return matricula;
-    }
-
-    public void setMatricula(int matricula) {
-        this.matricula = matricula;
-    }
 
     public int getNumeroIntegrante() {
         return numeroIntegrante;
